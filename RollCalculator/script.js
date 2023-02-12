5747993761e6589033931db1fc5afe5f2ebca5b2
Cathal <duane.cathal@gmail.com>
2023-02-12 18:06:45 +0100

    started calculator, need to link randomiser and start voice element. research how to toggle visual options

--- RollCalculator/script.js
+++ RollCalculator/script.js
@@ -2,6 +2,7 @@
 function generateRandomInteger(max) {
     return Math.floor(Math.random() * max) + 1;
 }
+
 let d100 = generateRandomInteger(100);
 let d20 = generateRandomInteger(20);
 let d12 = generateRandomInteger(12);
@@ -10,4 +11,13 @@ let d8 = generateRandomInteger(8);
 let d6 = generateRandomInteger(6);
 let d4 = generateRandomInteger(4);
 
-console.log("D20 roll: "+ d4);
+
+//clear input box
+function clear(){
+    document.getElementById('result').value = "";
+}
+
+//Dice Roll
+function roll(value){
+    document.getElementById('result').value += generateRandomInteger(value); 
+}
