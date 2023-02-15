44f5a8ecd6c2be3295a18227e57991deccbbbc7d
Cathal <duane.cathal@gmail.com>
2023-02-15 10:05:26 +0100

    got input and text area working. added color elements to dice rolls, add buttons for attributes

--- Roll Calulator/script.js
+++ Roll Calulator/script.js
@@ -1,30 +1,52 @@
 // Generate a random numbers for standard dice rolls 
 function generateRandomInteger(max) {
-    return Math.floor(Math.random() * max) + 1;
+    return Math.floor(Math.random() * max) + 1 + " ";
 }
 
 
 // //clear input box
-function clear(){
-    document.getElementById('result').value += 1;
+function clearInput(){
+    var getValue= document.getElementById("result");
+      if (getValue.value !="") {
+          getValue.value = "";
+      }
 }
+
+ if (target.value== 'clear input'){
+            target.value= "";
+       }
 
 //Add Dice Roll to input box
 function roll(value){
     document.getElementById('result').value += generateRandomInteger(value); 
+
+    if (value == "20") {
+        document.getElementById("result").style.color = "green"
+    } 
+
+    else if (value == "10") {
+        document.getElementById("result").style.color = "blue"
+    }
+    else if (value == "6") {
+        document.getElementById("result").style.color = "orange"
+    }
+    else if (value == "4") {
+        document.getElementById("result").style.color = "purple"
+    }
 }
 
 //Add value to input box
 function dis(value){
-    document.getElementById('result').value += value;
+    document.getElementById('result').value += value + " ";
+    document.getElementById("result").style.color = "black"
 }
 
 //equals function to calculate inputs
 function equals(){
-    var textArea = getElementById('history');
-    var x = getElementById('result').value;
+    var textArea = document.getElementById("history");
+    var x = document.getElementById("result").value;
     //eval() evaluates the string from x
     var y = eval(x);
-    clear();
+    clearInput();
     textArea.value = textArea.value + x + " = " + y + "\n";
 }
